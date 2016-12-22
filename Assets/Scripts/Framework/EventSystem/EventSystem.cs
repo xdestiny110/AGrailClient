304aebae1266ca49763010aa62f8e13ab6a2f043
Xdestiny <xdestiny@bit.edu.cn>
2016-12-22 18:24:20 +0800

    完成单向通知功能

---
+++ Assets/Scripts/Framework/EventSystem/EventSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Framework
+{
+    namespace EventSystem
+    {
+        public static class EventSystem
+        {
+            private static Dictionary<EventType, List<IEventListener>> maps = new Dictionary<EventType, List<IEventListener>>();
+
+            public static void Regist(EventType eventType, IEventListener listener)
+            {
+                if (!maps.ContainsKey(eventType))
+                    maps.Add(eventType, new List<IEventListener>() { listener });
+                else
+                    maps[eventType].Add(listener);
+            }
+
+            public static void UnRegist(EventType eventType, IEventListener listener)
+            {
+                if (maps.ContainsKey(eventType))                
+                    maps[eventType].Remove(listener);                
+            }
+
+            public static void Notify(EventType eventType, params object[] parameters)
+            {
+                if (maps.ContainsKey(eventType))
+                {
+                    var listeners = new List<IEventListener>(maps[eventType]);
+                    foreach (var v in listeners)
+                        v.OnEventTrigger(eventType, parameters);
+                }
+            }
+        }
+    }
+}
+
+
