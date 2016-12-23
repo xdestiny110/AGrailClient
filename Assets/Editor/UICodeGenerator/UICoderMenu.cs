f9edde3e8f13696c1e7b0eb2de9ba74bcff0b1a6
Xdestiny <xdestiny@bit.edu.cn>
2016-12-23 21:20:49 +0800

    生成UI工具需要继续改进

---
+++ Assets/Editor/UICodeGenerator/UICoderMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+namespace Editor.UIGen
+{
+    public class UICoderMenu
+    {
+        const string UIResourcePath = "Resources/UI";        
+
+        [MenuItem("AppEditor/GenerateUICode")]
+        public static void DoGenerate()
+        {
+            List<string> uiAssetsPath = EditorTool.AssetPathOfUnityFolder(UIResourcePath, ".prefab");
+            foreach(string uiAsset in uiAssetsPath)
+            {
+                UICoder.Instance.Generate(uiAsset);
+            }
+            
+            AssetDatabase.Refresh();
+        }
+    }
+}
