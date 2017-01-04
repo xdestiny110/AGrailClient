788ab5b22d084fc502d86edf20e5eec10eb78396
Xdestiny <xdestiny@bit.edu.cn>
2017-01-04 13:09:31 +0800

    添加单例基类 准备推翻以前的UI框架重来...主要是现在这套还是稍显复杂，弄套更简单一些的

---
+++ Assets/Scripts/Framework/Util/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Framework
+{
+    public class Singleton<T> where T : new()
+    {
+        protected static T instance = default(T);
+        protected static object locker = new object();
+        public static T Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    lock (locker)
+                    {
+                        if (instance == null)
+                            instance = new T();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        protected Singleton(){}
+    }
+}
+
+
