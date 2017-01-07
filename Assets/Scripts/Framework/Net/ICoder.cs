fcfc48e75bcb0ecaeef659e7ba70bcb691696f78
Xdestiny <xdestiny@bit.edu.cn>
2017-01-07 15:30:11 +0800

    解码编码接口与服务器配置接口

---
+++ Assets/Scripts/Framework/Net/ICoder.cs
@@ -0,0 +1,12 @@
+﻿using ProtoBuf;
+
+namespace Framework.Network
+{
+    public interface ICoder
+    {
+        bool Decode(byte[] stream, out IExtensible proto);
+        byte[] Encode(IExtensible proto);
+    }
+}
+
+
---
+++ Assets/Scripts/Framework/Net/IServerConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Framework.Network
+{
+    public interface IServerConfig
+    {
+        string ServerIP { get; }
+        int Port { get; }
+    }
+}
