963bd08b14bdbfcac04f75abf1c4024f8e3e92a5
Xdestiny <xdestiny@bit.edu.cn>
2017-01-09 20:45:08 +0800

    只要把生成代码部分的代码补上就OK了

---
+++ Assets/Editor/MessageCodeGenerator/MsgCodeGeneratorEditor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Framework.Message
+{
+    [CustomEditor(typeof(MsgCodeGenerator))]
+    public class MsgCodeGeneratorEditor : Editor
+    {
+        private ReorderableList reorderableList;
+
+        [MenuItem("Framework/Message Code Generate")]
+        public static void generateCode()
+        {
+            Selection.activeObject = MsgCodeGenerator.Instance;
+        }
+
+        void OnEnable()
+        {
+            var prop = serializedObject.FindProperty("msgtypes");
+            reorderableList = new ReorderableList(serializedObject, prop, true, true, true, true);
+            reorderableList.drawHeaderCallback = (rect) => { EditorGUI.LabelField(rect, "Message Type"); };
+            reorderableList.drawElementCallback = (rect, idx, isActive, isFocused) =>
+            {
+                var element = prop.GetArrayElementAtIndex(idx);
+                rect.y += 2;
+                EditorGUI.PropertyField(rect, element);
+            };            
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update();
+            reorderableList.DoLayoutList();
+            serializedObject.ApplyModifiedProperties();
+
+            if(GUILayout.Button("Generate Code"))
+            {
+                writeCode();
+            }
+        }
+
+        private void writeCode()
+        {
+
+        }
+
+    }
+}
+
+
---
+++ Assets/Scripts/Framework/MessageSystem/MsgCodeGenerator.cs
@@ -0,0 +1,41 @@
+﻿namespace Framework.Message
+{
+#if UNITY_EDITOR
+    using UnityEngine;
+    using UnityEditor;
+    using System.Collections.Generic;
+
+    [InitializeOnLoad]
+    public class MsgCodeGenerator : ScriptableObject
+    {
+        public const string configPath = "Assets/Editor/MessageCodeGenerator/Config.asset";
+        public const string messageTypePath = "Assets/Scripts/Framework/MessageSystem/MessageType.cs";
+
+        private static MsgCodeGenerator instance;
+        public static MsgCodeGenerator Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = AssetDatabase.LoadAssetAtPath<MsgCodeGenerator>(configPath);
+                    if(instance == null)
+                    {
+                        instance = CreateInstance<MsgCodeGenerator>();
+                        AssetDatabase.CreateAsset(instance, configPath);
+                        AssetDatabase.Refresh();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        [SerializeField]
+        [HideInInspector]
+        private List<string> msgtypes = new List<string>();
+
+    }
+#endif
+}
+
+
