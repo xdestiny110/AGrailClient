4efb99f175c27f3277b625b9ea6a0cf407812a59
Xdestiny <xdestiny@bit.edu.cn>
2017-01-16 22:30:41 +0800

    先凑合这解决一下卡顿的问题...

--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -3,6 +3,7 @@ using Framework.UI;
 using Framework.Message;
 using DG.Tweening;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace AGrail
 {   
@@ -21,25 +22,15 @@ namespace AGrail
         {
             set
             {
+                if(coroHandle != null)
+                {                    
+                    StopCoroutine(coroHandle);
+                    coroHandle = null;
+                }
                 for (int i = 0; i < content.childCount; i++)
                     Destroy(content.GetChild(i).gameObject);
-                if (value == null)
-                {                    
-                    loadingIcon.SetActive(true);
-                }
-                else
-                {
-                    loadingIcon.SetActive(false);
-                    foreach(var v in value)
-                    {
-                        //Debug.Log(Time.realtimeSinceStartup);
-                        var go = Instantiate(roomItemPrefab);
-                        //Debug.Log(Time.realtimeSinceStartup);
-                        go.transform.parent = content;
-                        var script = go.GetComponent<RoomItem>();
-                        script.RoomInfo = v;
-                    }
-                }
+                loadingIcon.SetActive(true);
+                coroHandle = StartCoroutine(addRoomItem(value));
             }
         }
 
@@ -77,6 +68,27 @@ namespace AGrail
                     roomInfos = Lobby.Instance.RoomInfo;
                     break;
             }
+        }
+
+        private Coroutine coroHandle = null;
+        private IEnumerator addRoomItem(List<network.RoomListResponse.RoomInfo> roomInfos)
+        {
+            if (roomInfos == null) yield break;
+            foreach (var v in roomInfos)
+            {
+                var go = Instantiate(roomItemPrefab);
+                go.transform.parent = content;
+                go.SetActive(false);
+                var script = go.GetComponent<RoomItem>();
+                script.RoomInfo = v;
+                //怒了，一帧只允许创建一个
+                //在没有统一管理的资源池与协程池前先这么凑合着
+                //以后搞成Bundle异步加载应该会好些
+                yield return null;
+            }
+            for(int i = 0; i < content.childCount; i++)            
+                content.GetChild(i).gameObject.SetActive(true);            
+            loadingIcon.SetActive(false);
         }
 
     }
