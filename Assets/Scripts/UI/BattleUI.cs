ac867c1248504b16a6d7fafbccf024db411fb2e7
Xdestiny <xdestiny@bit.edu.cn>
2017-01-19 23:57:45 +0800

    略累....明天继续堆逻辑...

--- Assets/Scripts/UI/BattleUI.cs
+++ Assets/Scripts/UI/BattleUI.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using Framework;
-using Framework.UI;
+﻿using Framework.UI;
 using Framework.Message;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using DG.Tweening;
 
 namespace AGrail
 {
     public class BattleUI : WindowsBase
     {
+        [SerializeField]
+        private Text turn;
+        [SerializeField]
+        private Transform[] morales = new Transform[2];
+        [SerializeField]
+        private Transform[] energy = new Transform[2];
+        [SerializeField]
+        private Transform[] grail = new Transform[2];
+        [SerializeField]
+        private Transform leftPlayerStatus;
+        [SerializeField]
+        private GameObject playerStatusPrefab;
+
+        [SerializeField]
+        private Texture2D[] icons = new Texture2D[3];
+
+        private List<PlayerStatus> players = new List<PlayerStatus>();
+
         public override WindowType Type
         {
             get
@@ -17,6 +37,7 @@ namespace AGrail
 
         public override void Awake()
         {
+            players.Clear();
             MessageSystem<MessageType>.Regist(MessageType.MoraleChange, this);
             MessageSystem<MessageType>.Regist(MessageType.GemChange, this);
             MessageSystem<MessageType>.Regist(MessageType.CrystalChange, this);
@@ -28,7 +49,13 @@ namespace AGrail
 
         public override void OnDestroy()
         {
-
+            players.Clear();
+            MessageSystem<MessageType>.UnRegist(MessageType.MoraleChange, this);
+            MessageSystem<MessageType>.UnRegist(MessageType.GemChange, this);
+            MessageSystem<MessageType>.UnRegist(MessageType.CrystalChange, this);
+            MessageSystem<MessageType>.UnRegist(MessageType.GrailChange, this);
+            MessageSystem<MessageType>.UnRegist(MessageType.PlayerIsReady, this);
+            MessageSystem<MessageType>.UnRegist(MessageType.PlayerNickName, this);
             base.OnDestroy();
         }
 
@@ -36,7 +63,19 @@ namespace AGrail
         {
             switch (eventType)
             {
-
+                case MessageType.MoraleChange:
+                    morales[(int)parameters[0]].DOScaleX(BattleData.Instance.Morale[(int)parameters[0]] / 15.0f, 0.2f);
+                    break;
+                case MessageType.GemChange:
+                    break;
+                case MessageType.CrystalChange:
+                    break;
+                case MessageType.GrailChange:
+                    break;
+                case MessageType.PlayerIsReady:
+                    break;
+                case MessageType.PlayerNickName:
+                    break;
             }
         }
 
---
+++ Assets/Scripts/UI/PlayerStatus.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AGrail
+{
+    public class PlayerStatus : MonoBehaviour
+    {
+        [SerializeField]
+        private Text userName;
+        [SerializeField]
+        private RawImage teamBG;
+        [SerializeField]
+        private RawImage heroIcon;
+        [SerializeField]
+        private RawImage heroProperty;
+        [SerializeField]
+        private Text heroName;
+        [SerializeField]
+        private Transform energy;
+        [SerializeField]
+        private Transform handCard;
+        [SerializeField]
+        private Transform heal;
+        [SerializeField]
+        private Transform basicAndExCards;
+        [SerializeField]
+        private Image border;
+        [SerializeField]
+        private Image readyIcon;
+
+        [SerializeField]
+        private Texture2D[] teamBGs = new Texture2D[3];
+        [SerializeField]
+        private Texture2D[] healIcons = new Texture2D[3];
+        [SerializeField]
+        private Texture2D[] properties = new Texture2D[5];
+        [SerializeField]
+        private Texture2D[] handAndHealIcons = new Texture2D[2];
+
+        public string UserName { set { userName.text = value; } }
+    }
+}
+
+
