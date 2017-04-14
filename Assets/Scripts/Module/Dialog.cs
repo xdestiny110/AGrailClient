249cc073b1600df098ebea387a239019af60592e
Xdestiny <xdestiny@bit.edu.cn>
2017-04-15 01:37:29 +0800

    修正Dialog中人物错误的问题 目前发现几个BUG 1. 进入战斗再推出后，showCardArea丢失 2. 有时候状态标识无法消除？不确定 下一步将人物弄进来，起码能在UI上先显示人物名称

--- Assets/Scripts/Module/Dialog.cs
+++ Assets/Scripts/Module/Dialog.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using Framework;
 using Framework.Message;
 using System;
+using System.Linq;
 
 namespace AGrail
 {
@@ -36,28 +37,43 @@ namespace AGrail
 
         public void OnEventTrigger(MessageType eventType, params object[] parameters)
         {
+            network.SinglePlayerInfo srcPlayer, dstPlayer;
             switch (eventType)
             {
                 case MessageType.HITMSG:
-                    var hitMsg = parameters[0] as network.HitMsg;                    
-                    Log += string.Format("{0}攻击了{1}" + Environment.NewLine, 
-                        BattleData.Instance.PlayerInfos[(int)hitMsg.src_id].nickname,
-                        BattleData.Instance.PlayerInfos[(int)hitMsg.dst_id].nickname);
+                    var hitMsg = parameters[0] as network.HitMsg;
+                    srcPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                        u => { return u != null && u.id == hitMsg.src_id; });
+                    dstPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                        u => { return u != null && u.id == hitMsg.dst_id; });
+                    Log += string.Format("{0}攻击了{1}" + Environment.NewLine, srcPlayer.nickname, dstPlayer.nickname);
                     break;
                 case MessageType.HURTMSG:
                     var hurtMsg = parameters[0] as network.HurtMsg;
-                    Log += string.Format("{0}对{1}造成{2}点伤害" + Environment.NewLine,
-                        BattleData.Instance.PlayerInfos[(int)hurtMsg.src_id].nickname,
-                        hurtMsg.dst_idSpecified ? BattleData.Instance.PlayerInfos[(int)hurtMsg.dst_id].nickname : "自己",
-                        hurtMsg.hurt);
+                    srcPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                        u => { return u != null && u.id == hurtMsg.src_id; });
+                    Log += srcPlayer.nickname;
+                    if (hurtMsg.dst_idSpecified)
+                    {
+                        dstPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                            u => { return u != null && u.id == hurtMsg.dst_id; });
+                        Log += "对" + dstPlayer.nickname;
+                    }
+                    Log += string.Format("造成{0}点伤害" + Environment.NewLine, hurtMsg.hurt);
                     break;
                 case MessageType.CARDMSG:
                     var cardMsg = parameters[0] as network.CardMsg;
-                    Log += string.Format(
-                        (cardMsg.type == (uint)network.CardMsgType.CM_SHOW) ?
-                        "{0}对{1}展示了" : "{0}对{1}使用了",
-                        BattleData.Instance.PlayerInfos[(int)cardMsg.src_id].nickname,
-                        cardMsg.dst_idSpecified ? BattleData.Instance.PlayerInfos[(int)cardMsg.dst_id].nickname : "自己");                    
+                    srcPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                        u => { return u != null && u.id == cardMsg.src_id; });
+                    Log += srcPlayer.nickname;
+                    if (cardMsg.dst_idSpecified)
+                    {
+                        dstPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                            u => { return u != null && u.id == cardMsg.dst_id; });
+                        Log += "对" + dstPlayer.nickname + "使用了";
+                    }
+                    else
+                        Log += "展示了";                       
                     foreach (var v in cardMsg.card_ids)
                     {
                         var c = new Card(v);
@@ -66,16 +82,24 @@ namespace AGrail
                     Log += Environment.NewLine;
                     break;
                 case MessageType.SKILLMSG:
-                    var skillMsg = parameters[0] as network.SkillMsg;                    
-                    Log += BattleData.Instance.PlayerInfos[(int)skillMsg.src_id].nickname;
+                    var skillMsg = parameters[0] as network.SkillMsg;
+                    srcPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                        u => { return u != null && u.id == skillMsg.src_id; });
+                    Log += srcPlayer.nickname;
                     if (skillMsg.dst_ids.Count > 0) Log += "对";
                     foreach (var v in skillMsg.dst_ids)
-                        Log += BattleData.Instance.PlayerInfos[(int)v].nickname + ",";
+                    {
+                        dstPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                            u => { return u != null && u.id == v; });
+                        Log += dstPlayer.nickname + ",";
+                    }                        
                     Log += string.Format("使用了技能{0}" + Environment.NewLine, skillMsg.skill_id);                    
                     break;
                 case MessageType.GOSSIP:
                     var gossip = parameters[0] as network.Gossip;
-                    Log += string.Format("{0}: {1}" + Environment.NewLine, BattleData.Instance.PlayerInfos[(int)gossip.id].nickname, gossip.txt);
+                    srcPlayer = BattleData.Instance.PlayerInfos.DefaultIfEmpty(null).FirstOrDefault(
+                        u => { return u != null && u.id == gossip.id; });
+                    Log += string.Format("{0}: {1}" + Environment.NewLine, srcPlayer.nickname, gossip.txt);
                     break;
             }
         }
