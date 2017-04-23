eb6df36769f730655ceb230d4f8f01a263c90b00
Xdestiny <xdestiny@bit.edu.cn>
2017-04-23 21:47:53 +0800

    添加几种基础的行动

--- Assets/Scripts/Module/Role/RoleBase.cs
+++ Assets/Scripts/Module/Role/RoleBase.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using network;
+using System.Collections.Generic;
+using Framework.Network;
 
 namespace AGrail
 {
@@ -13,5 +16,145 @@ namespace AGrail
         public virtual bool HasBlue { get { return false; } }
         public virtual bool HasCoverd { get { return false; } }
         public virtual string Knelt { get { return null; } }
+
+        public virtual void Attack(uint srcID, Card card, uint dstID)
+        {
+            //普通攻击
+            sendActionMsg(BasicActionType.ACTION_ATTACK, srcID, new List<uint>() { dstID }, new List<uint>() { card.ID });
+        }
+
+        public virtual void Magic(uint srcID, Card card, uint dstID)
+        {
+            //基础法术
+            sendActionMsg(BasicActionType.ACTION_MAGIC, srcID, new List<uint>() { dstID }, new List<uint>() { card.ID });
+        }
+
+        public virtual void Weaken(uint srcID, List<uint> args = null)
+        {
+            //被虚弱 
+            sendReponseMsg((uint)BasicRespondType.RESPOND_WEAKEN, srcID, null, null, args);
+        }
+
+        public virtual void MoDaned(uint srcID, Card card = null)
+        {
+            //被魔弹
+            if (card != null)
+            {
+                if (card.Element != Card.CardElement.light)
+                    sendReponseMsg((uint)BasicRespondType.RESPOND_BULLET, srcID, null, new List<uint>() { card.ID }, new List<uint>() { 0 });
+                else
+                    sendReponseMsg((uint)BasicRespondType.RESPOND_BULLET, srcID, null, new List<uint>() { card.ID }, new List<uint>() { 1 });
+            }
+            else
+                sendReponseMsg((uint)BasicRespondType.RESPOND_BULLET, srcID, null, null, new List<uint>() { 2 });
+        }
+
+        public virtual void Heal(uint srcID, List<uint> args = null)
+        {
+            //治疗 
+            sendReponseMsg((uint)BasicRespondType.RESPOND_HEAL, srcID, null, null, args);
+        }
+
+        public virtual void Drop(uint srcID, List<uint> NIDs)
+        {
+            //弃牌
+            sendReponseMsg((uint)BasicRespondType.RESPOND_DISCARD, srcID, null, NIDs, new List<uint>() { 1 });
+        }
+
+        public virtual void AttackedReply(uint srcID, Card card = null, uint? dstID = null)
+        {
+            //应战
+            if (!dstID.HasValue && card == null)
+            {
+                //放弃
+                sendReponseMsg((uint)BasicRespondType.RESPOND_REPLY_ATTACK, srcID, null, null, new List<uint>() { 2 });
+            }
+            else
+            {
+                if (dstID.HasValue && card.Element != Card.CardElement.light)
+                {
+                    //应战
+                    sendReponseMsg((uint)BasicRespondType.RESPOND_REPLY_ATTACK, srcID, new List<uint>() { dstID.Value }, new List<uint>() { card.ID }, new List<uint> { 0 });
+                }
+                else
+                {
+                    //圣光
+                    sendReponseMsg((uint)BasicRespondType.RESPOND_REPLY_ATTACK, srcID, null, new List<uint>() { card.ID }, new List<uint>() { 1 });
+                }
+            }
+        }
+
+        public virtual void Buy(uint srcID, uint gemNum, uint cristalNum)
+        {
+            //购买        
+            sendActionMsg(BasicActionType.ACTION_SPECIAL, srcID, null, null, 0, new List<uint>() { gemNum, cristalNum });
+        }
+
+        public virtual void Synthetize(uint srcID, uint gemNum, uint cristalNum)
+        {
+            //合成
+            sendActionMsg(BasicActionType.ACTION_SPECIAL, srcID, null, null, 1, new List<uint>() { gemNum, cristalNum });
+        }
+
+        public virtual void Extract(uint srcID, uint gemNum, uint cristalNum)
+        {
+            //提炼
+            sendActionMsg(BasicActionType.ACTION_SPECIAL, srcID, null, null, 2, new List<uint>() { gemNum, cristalNum });
+        }
+
+        public virtual void UseSkill(uint skillID, uint srcID, List<uint> dstID = null, List<uint> cardIds = null, List<uint> args = null)
+        {
+
+        }
+
+        protected void sendActionMsg(BasicActionType actionType, uint srcID, List<uint> dstID = null, List<uint> cardIDs = null, uint? actionID = null, List<uint> args = null)
+        {
+            Action proto = new Action() { action_type = (uint)actionType, src_id = srcID };
+            if (dstID != null && dstID.Count > 0)
+            {
+                foreach (var v in dstID)
+                    proto.dst_ids.Add(v);
+            }
+
+            if (cardIDs != null && cardIDs.Count > 0)
+            {
+                foreach (var v in cardIDs)
+                    proto.card_ids.Add(v);
+            }
+
+            if (actionID.HasValue)
+            {
+                proto.action_id = actionID.Value;
+                if (args != null && args.Count > 0)
+                {
+                    foreach (var v in args)
+                        proto.args.Add(v);
+                }
+            }
+            
+            GameManager.TCPInstance.Send(new Protobuf() { Proto = proto, ProtoID = ProtoNameIds.ACTION });
+        }
+
+        protected void sendReponseMsg(uint respondID, uint srcID, List<uint> dstID = null, List<uint> cardIDs = null, List<uint> args = null)
+        {
+            Respond proto = new Respond() { respond_id = respondID, src_id = srcID };
+            if (dstID != null)
+            {
+                foreach (var v in dstID)
+                    proto.dst_ids.Add(v);
+            }
+            if (cardIDs != null)
+            {
+                foreach (var v in cardIDs)
+                    proto.card_ids.Add(v);
+            }
+            if (args != null)
+            {
+                foreach (var v in args)
+                    proto.args.Add(v);
+            }
+
+            GameManager.TCPInstance.Send(new Protobuf() { Proto = proto, ProtoID = ProtoNameIds.RESPOND });
+        }
     }
 }
