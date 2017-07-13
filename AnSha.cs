ea90af92477fcb05bb0a86ca96644986a477577c
jianm1996 <2385992390@qq.com>
2017-07-13 21:09:13 +0800

    Add files via upload

---
+++ AnSha.cs
@@ -0,0 +1,121 @@
+using System.Collections.Generic;
+using Framework.Message;
+
+namespace AGrail
+{
+    public class AnSha : RoleBase
+    {
+        public override RoleID RoleID
+        {
+            get
+            {
+                return RoleID.AnSha;
+            }
+        }
+
+        public override string RoleName
+        {
+            get
+            {
+                return "暗杀者";
+            }
+        }
+
+        public override Card.CardProperty RoleProperty
+        {
+            get
+            {
+                return Card.CardProperty.技;
+            }
+        }
+
+        public override string Knelt
+        {
+            get
+            {
+                return "QianXing";
+            }
+        }
+
+        public AnSha()
+        {
+            for (uint i = 501; i <= 503; i++)
+                Skills.Add(i, Skill.GetSkill(i));
+        }
+
+        public override bool CanSelect(uint uiState, Card card, bool isCovered)
+        {
+            if (uiState == 502 && card.Element == Card.CardElement.water)
+                return true;                
+            return base.CanSelect(uiState, card, isCovered);
+        }
+
+        public override bool CheckOK(uint uiState, List<uint> cardIDs, List<uint> playerIDs, uint? skillID)
+        {
+            switch (uiState)
+            {
+                case 502:
+                    if (cardIDs.Count > 0)
+                        return true;
+                    return false;
+                case 503:
+                    return true;
+            }
+            return base.CheckOK(uiState, cardIDs, playerIDs, skillID);
+        }
+
+        public override bool CheckCancel(uint uiState, List<uint> cardIDs, List<uint> playerIDs, uint? skillID)
+        {
+            switch (uiState)
+            {
+                case 502:
+                case 503:
+                    return true;
+            }
+            return base.CheckCancel(uiState, cardIDs, playerIDs, skillID);
+        }
+
+        public override uint MaxSelectCard(uint uiState)
+        {
+            if (uiState == 502)
+                return 7;
+            return base.MaxSelectCard(uiState);
+        }
+
+        public override void UIStateChange(uint state, UIStateMsg msg, params object[] paras)
+        {
+            switch (state)
+            {
+                case 502:
+                    OKAction = () => 
+                    {
+                        sendReponseMsg(state, BattleData.Instance.MainPlayer.id, null, BattleData.Instance.Agent.SelectCards);
+                        BattleData.Instance.Agent.FSM.ChangeState<StateIdle>(UIStateMsg.Init, true);
+                    };
+                    CancelAction = () => 
+                    {
+                        sendReponseMsg(state, BattleData.Instance.MainPlayer.id, null, null);
+                        BattleData.Instance.Agent.FSM.ChangeState<StateIdle>(UIStateMsg.Init, true);
+                    };
+                    MessageSystem<MessageType>.Notify(MessageType.SendHint, "水影: 请选择舍弃的水系牌");
+                    return;
+                case 503:
+                    OKAction = () =>
+                    {
+                        IsStart = true;
+                        sendReponseMsg(state, BattleData.Instance.MainPlayer.id, null, null, new List<uint>() { 1 });
+                        BattleData.Instance.Agent.FSM.ChangeState<StateIdle>(UIStateMsg.Init, true);
+                    };
+                    CancelAction = () =>
+                    {
+                        sendReponseMsg(state, BattleData.Instance.MainPlayer.id, null, null, new List<uint>() { 0 });
+                        BattleData.Instance.Agent.FSM.ChangeState<StateIdle>(UIStateMsg.Init, true);
+                    };
+                    MessageSystem<MessageType>.Notify(MessageType.SendHint, "是否发动潜行");
+                    return;
+            }
+            base.UIStateChange(state, msg, paras);
+        }
+
+    }
+}
---
+++ RoleFactory.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace AGrail
+{
+    public static class RoleFactory
+    {
+        private static Dictionary<string, RoleBase> pool = new Dictionary<string, RoleBase>();
+
+        public static RoleBase Create(string roleName)
+        {
+            if (pool.ContainsKey(roleName))
+                return pool[roleName];
+            var type = Type.GetType("AGrail." + roleName);
+            if(type == null)
+                Debug.LogWarningFormat("Cannot create role! Rolename = {0}", roleName);            
+            var r = (type == null) ? new Blank() : Activator.CreateInstance(type) as RoleBase;
+            pool.Add(roleName, r);
+            return r;
+        }
+
+        public static RoleBase Create(RoleID roleID)
+        {
+            return Create(roleID.ToString());
+        }
+
+        public static RoleBase Create(uint roleID)
+        {
+            return Create((RoleID)roleID);
+        }
+    }
+
+    public enum RoleID
+    {
+        Blank = 0,
+        JianSheng,
+        KuangZhan,
+        GongNv,
+        FengYin,
+        AnSha,
+        ShengNv,
+        TianShi,
+        MoDao,
+        MoJian,
+        ShengQiang,
+        YuanSu,
+        MaoXian,
+        SiLing,
+        ZhongCai,
+        ShenGuan,
+        QiDao,
+        XianZhe,
+        LingFu,
+        JianDi,
+        GeDouJia,
+        YongZhe,
+        LingHun,
+        WuNv,
+        DieWu,
+        NvWuShen,
+        MoGong,
+        YingLingRenXing,
+        HongLian,
+        MoQiang,
+        MoNv,
+        ShiRen,
+        spMoDao=108,
+    }
+}
+
