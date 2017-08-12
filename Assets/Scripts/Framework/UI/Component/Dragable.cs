815772d4f2e7d4670ecb77fc52e218d57d4aaa67
Xdestiny <xdestiny@bit.edu.cn>
2017-08-13 00:30:51 +0800

    添加拖拽是释放控件脚本 还需要对应的Editor

---
+++ Assets/Scripts/Framework/UI/Component/Dragable.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+namespace Framework.UI
+{
+    public class Dragable : UIBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, ICanvasRaycastFilter
+    {
+        private Canvas rootCanvas;
+        private bool isRaycastValid = true;
+
+        [SerializeField]
+        public class OnDragEndEventTriiger : UnityEvent<GameObject, PointerEventData> { }
+
+        [FormerlySerializedAs("onEndDrag")]
+        [SerializeField]
+        private OnDragEndEventTriiger onEndDrag;
+        public OnDragEndEventTriiger OnEndDragEvent
+        {
+            get
+            {
+                return onEndDrag;
+            }
+
+            set
+            {
+                onEndDrag = value;
+            }
+        }
+
+        protected override void Awake()
+        {
+            rootCanvas = transform.root.GetComponent<Canvas>();
+            if (rootCanvas == null)
+                Debug.LogErrorFormat("Cannot find root canvas! name = {0}", gameObject.name);
+            base.Awake();
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            transform.parent = rootCanvas.transform;
+            transform.SetAsLastSibling();
+            isRaycastValid = false;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            Vector3 mouseWorldPosition;
+            if (RectTransformUtility.ScreenPointToWorldPointInRectangle(transform as RectTransform, eventData.position, 
+                eventData.pressEventCamera, out mouseWorldPosition))
+            {
+                gameObject.transform.position = mouseWorldPosition;
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            var go = eventData.pointerCurrentRaycast.gameObject;
+            if (go != null && go.GetComponentInParent<Dropable>() != null)
+                go.GetComponentInParent<Dropable>().OnDrop(gameObject, eventData);
+            isRaycastValid = true;
+            if (OnEndDragEvent != null)
+                OnEndDragEvent.Invoke(go, eventData);
+        }
+
+        public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+        {
+            return isRaycastValid;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Framework/UI/Component/Dropable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+namespace Framework.UI
+{
+    public class Dropable : UIBehaviour
+    {
+        [SerializeField]
+        public class OnDropEventTriiger : UnityEvent<GameObject, PointerEventData> { }
+
+        [FormerlySerializedAs("onDrop")]
+        [SerializeField]
+        private OnDropEventTriiger onDrop = new OnDropEventTriiger();
+
+        public OnDropEventTriiger OnDropEvent
+        {
+            get
+            {
+                return onDrop;
+            }
+
+            set
+            {
+                onDrop = value;
+            }
+        }
+
+        public void OnDrop(GameObject go, PointerEventData d)
+        {
+            if (OnDropEvent != null)
+                OnDropEvent.Invoke(go, d);
+        }
+    }
+}
