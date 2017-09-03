ffcd02ce70f1ce443ce531ee8138583fb8679f18
Xdestiny <xdestiny@bit.edu.cn>
2017-09-03 23:20:13 +0800

    生成UI代码

--- Assets/Scripts/Framework/UI/WindowType.cs
+++ Assets/Scripts/Framework/UI/WindowType.cs
@@ -8,16 +8,17 @@
         ChooseArgsUI,
         ChooseEnergy,
         CreateRoomUI,
+        DisConnectedPoll,
         InfomationUI,
         InputBox,
         Lobby,
         LoginBox,
         OptionsUI,
+        PlayerIco,
         ReadyRoom,
         RoleChoose31,
         RoleChooseAny,
         SpecialActionUI,
         Loading,
-        DisConnectedPoll
     }
 }
