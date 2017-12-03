025b88b890a62786857be7ff6feaaea7273c4f67
竹间月 <2385992390@qq.com>
2017-12-03 11:36:05 +0800

    美化公告和图鉴

--- Assets/Scripts/UI/HandBook.cs
+++ Assets/Scripts/UI/HandBook.cs
@@ -16,6 +16,18 @@ namespace AGrail
     {
         [SerializeField]
         private Transform SkillArea;
+        [SerializeField]
+        private Button button0;
+        [SerializeField]
+        private Button button1;
+        [SerializeField]
+        private Button button2;
+        [SerializeField]
+        private Button button3;
+        [SerializeField]
+        private Button button4;
+        [SerializeField]
+        private Button button5;
 
         public override WindowType Type
         {
@@ -27,22 +39,38 @@ namespace AGrail
 
         public override void Awake()
         {
+            button0.onClick.AddListener(delegate { GenerateHero(0); });
+            button1.onClick.AddListener(delegate { GenerateHero(1); });
+            button2.onClick.AddListener(delegate { GenerateHero(2); });
+            button3.onClick.AddListener(delegate { GenerateHero(3); });
+            button4.onClick.AddListener(delegate { GenerateHero(4); });
+            button5.onClick.AddListener(delegate { GenerateHero(5); });
+            GenerateHero(0);
+            base.Awake();
+        }
+        public void GenerateHero(uint p)
+        {
+            for (int i = 0; i < SkillArea.childCount; i++)
+            {
+                Destroy(SkillArea.GetChild(i).gameObject);
+            }
             var prefab = AssetBundleManager.Instance.LoadAsset("lobby", "HeroIco");
-            for (uint a=1;a<33;a++)
+            for (uint a = 1; a < 33; a++)
             {
                 uint i = a;
                 if (a == 32) i = 108;
+                var role = RoleFactory.Create(i);
+                if(p==0 || role.RoleProperty == (Card.CardProperty)p)
+                { 
                 var go = Instantiate(prefab);
                 go.transform.SetParent(SkillArea);
                 go.transform.localScale = new Vector3(1, 1, 1);
-                var role = RoleFactory.Create(i);
                 go.transform.GetComponent<Image>().sprite = AssetBundleManager.Instance.LoadAsset<Sprite>("hero_h", i.ToString() + "h");
                 go.transform.GetComponent<Button>().onClick.AddListener(
                     () => { GameManager.UIInstance.PushWindow(Framework.UI.WindowType.InfomationUI, Framework.UI.WinMsg.None, -1, Vector3.zero, role); });
                 go.transform.GetChild(0).GetComponent<Text>().text = role.RoleName;
+                }
             }
-            base.Awake();
         }
-
     }
 }
