809a437d114c233459a6dea3fe18a2f460aadeb2
Xdestiny <xdestiny@bit.edu.cn>
2017-12-23 17:06:44 +0800

    修正字体乱码问题（待验证）

---
+++ Assets/Scripts/Util/UIFontDirty.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AGrail
+{
+    public class UIFontDirty : MonoBehaviour
+    {
+        bool isDirty = false;
+        Font dirtyFont = null;
+
+        void Awake()
+        {
+            Font.textureRebuilt += f =>
+            {
+                isDirty = true;
+                dirtyFont = f;
+            };
+        }
+
+        void LateUpdate()
+        {
+            if (isDirty)
+            {
+                isDirty = false;
+                foreach (Text text in FindObjectsOfType<Text>())
+                {
+                    if (text.font == dirtyFont)
+                    {
+                        text.FontTextureChanged();
+                    }
+                }
+                dirtyFont = null;
+            }
+        }
+    }
+}
+
+
